1da3ba50a2ca85d964c13cd66e2d6241f14157de
DezesTheOne <hedvika.reichlova@gmail.com>
2023-10-05 14:42:43 +0200

    propojeni_hash

--- seznam.js
+++ seznam.js
@@ -110,13 +110,13 @@ seznamFilmu.innerHTML = '';
 
 filmy.forEach((film) => {
   seznamFilmu.innerHTML += `
-	<div class="col"> 
-	<div class="card">
-	<img src="${film.plakat.url}" width="${film.plakat.sirka}" height="${film.plakat.vyska}" class="card-img-top" alt="plakát" />
-	<div class="card-body">
-		<h5 class="card-title">${film.nazev}</h5>
-		<p class="card-text">${film.ochutnavka}</p>
-		<a href="${film.id}.html" class="btn btn-primary">Přehrát</a>
- </div>
-</div>`;
+		<div class="col"> 
+		<div class="card">
+		<img src="${film.plakat.url}" width="${film.plakat.sirka}" height="${film.plakat.vyska}" class="card-img-top" alt="plakát" />
+		<div class="card-body">
+			<h5 class="card-title">${film.nazev}</h5>
+			<p class="card-text">${film.ochutnavka}</p>
+			<a href="film.html#${film.id}" class="btn btn-primary">Přehrát</a>
+	 </div>
+	 </div>`;
 });
